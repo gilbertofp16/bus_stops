ea8094597b2f6ed5dfb646a2e6823344d5b0ffca
gilbertofp16 <gilbertofp16@gmail.com>
2016-11-06 19:43:15 +0100

    all

--- stops/views.py
+++ stops/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 import requests, json
 import find_centroids
+from pathlib import Path
 
 # method to capture GET and POST for search in Github repositories
 def map_show(request):
 
-    find_centroids.start_find_points()
+    index = Path("stops/templates/index.html")
+    if not index.is_file():
+        find_centroids.start_find_points()
 
     if request.method == 'GET':
         return render(request, 'index.html')
